12502f694dfd5858192b071b3331343d7f4a74b4
Chris Kong <christopher.j.kong@gmail.com>
2015-10-23 17:10:10 +0800

    Initial Commit

--- Wasei/AppDelegate.swift
+++ Wasei/AppDelegate.swift
@@ -15,7 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    // Override point for customization after application launch.
+    
+    // Change the navigation bar's appearance
+    UINavigationBar.appearance().barTintColor = UIColor(red: 239.0/255.0, green: 108.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+    
+    if let barFont = UIFont(name: "Avenir-Light", size: 21.0)
+    {
+      UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: barFont]
+    }
+    
+    // Change the tab bar's appearance
+    UITabBar.appearance().barTintColor = UIColor(red: 66.0/255.0, green: 66.0/255.0, blue: 66.0/255.0, alpha: 1.0)
+    
     return true
   }
 
