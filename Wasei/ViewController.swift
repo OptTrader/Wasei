7ed1c9b3b1ec9c0372458bca456affe4bca8f58c
Chris Kong <christopher.j.kong@gmail.com>
2015-10-23 16:47:41 +0800

    Initial Commit

---
+++ Wasei/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wasei
+//
+//  Created by Chris Kong on 10/23/15.
+//  Copyright © 2015 Chris Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
