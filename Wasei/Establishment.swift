b436500f476ee6f1b1660391d95723bbe631c980
Chris Kong <christopher.j.kong@gmail.com>
2015-10-23 17:37:32 +0800

    Added ExploreTableVC

---
+++ Wasei/Establishment.swift
@@ -0,0 +1,15 @@
+//
+//  Establishment.swift
+//  Wasei
+//
+//  Created by Chris Kong on 10/23/15.
+//  Copyright © 2015 Chris Kong. All rights reserved.
+//
+
+import Foundation
+
+class Establishment
+{
+  
+  
+}
