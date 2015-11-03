a9a2380b7f2a99f2ec7de49140e011f7872bce9a
Chris Kong <christopher.j.kong@gmail.com>
2015-11-03 11:47:28 +0800

    Latest commit

---
+++ Wasei/ExploreDetailsTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ExploreDetailsTableViewCell.swift
+//  Wasei
+//
+//  Created by Chris Kong on 10/31/15.
+//  Copyright © 2015 Chris Kong. All rights reserved.
+//
+
+import UIKit
+
+class ExploreDetailsTableViewCell: UITableViewCell
+{
+  @IBOutlet weak var fieldLabel: UILabel!
+  @IBOutlet weak var valueLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+
+  
+}
---
+++ Wasei/ExploreDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreDetailsViewController.swift
+//  Wasei
+//
+//  Created by Chris Kong on 10/31/15.
+//  Copyright © 2015 Chris Kong. All rights reserved.
+//
+
+import UIKit
+
+class ExploreDetailsViewController: UIViewController
+{
+  @IBOutlet weak var placeImageView: UIImageView!
+  @IBOutlet weak var tableView: UITableView!
+
+  
+  
+}
